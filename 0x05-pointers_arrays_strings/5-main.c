6c9db7122502c27028a81f35f760d0195fe00956
Ebuwa uyi <ebuwauyi088@gmail.com>
2023-10-17 19:29:02 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char s[10] = "My School";
+
+	char s[10] = "My School";
+	rev_string(s);
+	printf("%s\n", s);
+	return (0);
+}
